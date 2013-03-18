36b245a6e4b469e5ba59c6059528616bc52fc4d1
James Perkins <jperkins@redhat.com>
2013-03-18 14:59:54 -0700

    Upgrade versions.

--- src/main/java/org/jboss/as/forge/CliCommandCompleter.java
+++ src/main/java/org/jboss/as/forge/CliCommandCompleter.java
@@ -71,11 +71,11 @@ public class CliCommandCompleter implements CommandCompleter {
         cmdRegistry.registerHandler(new DeploymentInfoHandler(context), "deployment-info");
 
         // batch commands
-        cmdRegistry.registerHandler(new BatchHandler(), "batch");
+        cmdRegistry.registerHandler(new BatchHandler(context), "batch");
         cmdRegistry.registerHandler(new BatchDiscardHandler(), "discard-batch");
         cmdRegistry.registerHandler(new BatchListHandler(), "list-batch");
         cmdRegistry.registerHandler(new BatchHoldbackHandler(), "holdback-batch");
-        cmdRegistry.registerHandler(new BatchRunHandler(), "run-batch");
+        cmdRegistry.registerHandler(new BatchRunHandler(context), "run-batch");
         cmdRegistry.registerHandler(new BatchClearHandler(), "clear-batch");
         cmdRegistry.registerHandler(new BatchRemoveLineHandler(), "remove-batch-line");
         cmdRegistry.registerHandler(new BatchMoveLineHandler(), "move-batch-line");
