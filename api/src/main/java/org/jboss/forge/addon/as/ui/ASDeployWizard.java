a57f63e990434a63e1c90deaddc6282b0cf6c7d4
Jeremie Lagarde <jer@printstacktrace.org>
2014-01-07 00:49:13 +0100

    Adding api commands

---
+++ api/src/main/java/org/jboss/forge/addon/as/ui/ASDeployWizard.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.addon.as.ui;
+
+import org.jboss.forge.addon.ui.wizard.UIWizard;
+
+/**
+ * Deploy an application in the application server command.
+ * 
+ * @author Jeremie Lagarde
+ */
+public interface ASDeployWizard extends UIWizard
+{
+
+}
