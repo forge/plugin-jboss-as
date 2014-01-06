12113e3f6e327c78340eeb12e325791d7a477005
Jeremie Lagarde <jer@printstacktrace.org>
2014-01-06 23:02:37 +0100

    Adding provider spi

---
+++ spi/src/main/java/org/jboss/forge/addon/as/spi/ApplicationServerProvider.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.addon.as.spi;
+
+import org.jboss.forge.addon.projects.ProjectFacet;
+import org.jboss.forge.addon.ui.UIValidator;
+
+/**
+ * Provides an implementation of Application Server connector to provide management operations.
+ * 
+ * @author Jeremie Lagarde
+ */
+public interface ApplicationServerProvider extends ProjectFacet, UIValidator
+{
+   /**
+    * Return the name for this {@link ApplicationServerProvider}
+    * 
+    * Ex: JBossAS7
+    */
+   String getName();
+
+   /**
+    * Return the description for this {@link ApplicationServerProvider}
+    * 
+    * Ex: JBoss AS7
+    */
+   String getDescription();
+
+}
